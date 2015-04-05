5c7f1664423c62f36350746a7d64225572330bb3
nkshr <naokasa@outlook.jp>
2015-04-05 09:11:29 +0900

    a first commit

---
+++ simulated_annealing.cpp
@@ -0,0 +1,89 @@
+#define ALFA 0.5
+
+#include <iostream>
+
+using namespace std;
+
+double annealing(double startState, int maxIter, double goalE);
+double EVAL(double state);
+double NEIGHBOUR(double state);
+int get_random(int min, int max);
+double PROBABILITY(double e1, double e2, double t);
+double TEMPERATURE(double r);
+double random();
+
+int main(int argc, char **argv)
+{
+	FILE *gp;
+	gp = _popen("gnuplot -persist", "w");
+	fprintf(gp, "set title \"simulated annealing\"\n");
+	fprintf(gp, "set xrange[-4:8]\n");
+	fprintf(gp, "set yrange[-50:15]\n");
+	fprintf(gp, "set grid\n");
+	fprintf(gp, "plot x*x*x*x - 7*x*x*x + 5*x*x + 31*x - 30\n");
+	double result = annealing(-1000, 10000, -40);
+	cout << "result: " << result << endl;
+	return 0;
+}
+
+double annealing(double startState, int maxIter, double goalE)
+{
+	double state, bestState, nextState, e, bestE, nextE;
+	state = startState;
+	e = EVAL(state);
+	bestState = state;
+	bestE = e;
+	for (int iter = 0; iter < maxIter; iter++)
+	{
+		cout << iter << ": " << state << endl;
+		nextState = NEIGHBOUR(state);
+		nextE = EVAL(nextState);
+		if (nextE < bestE)
+		{
+			bestState = nextState;
+			bestE = nextE;
+			if (bestE <= goalE)
+				return bestState;
+		}
+		if (random() <= PROBABILITY(e, nextE, TEMPERATURE((double)iter / maxIter)))
+		{
+			state = nextState;
+			e = nextE;
+		}
+	}
+	return bestState;
+}
+
+double EVAL(double state)
+{
+	double x = state;
+	return pow(x, 4) - 7 * pow(x, 3) + 5 * pow(x, 2) + 31 * x - 30;
+}
+
+double NEIGHBOUR(double state)
+{
+	return state + (0.1) * get_random(-10, 10);
+}
+
+int get_random(int min, int max)
+{
+	return min + (int)(rand()*(max - min + 1.0) / (1.0 + RAND_MAX));
+}
+
+double PROBABILITY(double e1, double e2, double t)
+{
+	if (e1 >= e2)
+		return 1;
+	else
+		return exp((e1 - e2) / t);
+}
+
+double random()
+{
+	return 0.1 * get_random(0, 10);
+}
+
+double TEMPERATURE(double r)
+{
+	return pow(ALFA, r);
+}
