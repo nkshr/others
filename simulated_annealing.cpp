0c640c2568cbb71fc4868c8cb836dc503ca98e15
nkshr <naokasa@outlook.jp>
2015-04-05 10:57:31 +0900

    minor bugs fixed.

--- simulated_annealing.cpp
+++ simulated_annealing.cpp
@@ -21,8 +21,13 @@ int main(int argc, char **argv)
 	fprintf(gp, "set yrange[-50:15]\n");
 	fprintf(gp, "set grid\n");
 	fprintf(gp, "plot x*x*x*x - 7*x*x*x + 5*x*x + 31*x - 30\n");
-	double result = annealing(-1000, 10000, -40);
-	cout << "result: " << result << endl;
+	double result;
+	for (;;)
+	{
+		result = annealing(100, 1000000, -400);
+		cout << "result: " << result << endl;
+		break;
+	}
 	return 0;
 }
 
@@ -35,7 +40,7 @@ double annealing(double startState, int maxIter, double goalE)
 	bestE = e;
 	for (int iter = 0; iter < maxIter; iter++)
 	{
-		cout << iter << ": " << state << endl;
+		//cout << iter << ": " << state << endl;
 		nextState = NEIGHBOUR(state);
 		nextE = EVAL(nextState);
 		if (nextE < bestE)
@@ -62,7 +67,10 @@ double EVAL(double state)
 
 double NEIGHBOUR(double state)
 {
-	return state + (0.1) * get_random(-10, 10);
+	if (get_random(-10, 9) >= 0)
+		return state + 0.1;
+	else
+		return state - 0.1;
 }
 
 int get_random(int min, int max)
@@ -72,15 +80,19 @@ int get_random(int min, int max)
 
 double PROBABILITY(double e1, double e2, double t)
 {
+	//cout << t << endl;
 	if (e1 >= e2)
 		return 1;
 	else
+	{
+		//cout << exp((e1 - e2) / t) << endl;
 		return exp((e1 - e2) / t);
+	}
 }
 
 double random()
 {
-	return 0.1 * get_random(0, 10);
+	return 0.1 * get_random(0, 1);
 }
 
 double TEMPERATURE(double r)
