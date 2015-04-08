173884cd4e8ef0fb0927e48f30a035b45ebb1547
nkshr <naokasa@outlook.jp>
2015-04-08 18:00:26 +0900

    now coding.

--- gauss_newton_method.cpp
+++ gauss_newton_method.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <memory.h>
 
 using namespace std;
 
@@ -13,18 +14,18 @@ int iter = 0;
 
 
 
-double calc_gamma(double b[2], double dst[7])
+double calc_gamma(double beta[2], double dst[7])
 {
 	for(int i = 0; i < 7; ++i)
-		dst[i] = x[i][1] - ((b[0]*x[i][0]) / (b[1]+x[i][0]));
+		dst[i] = x[i][1] - ((beta[0]*x[i][0]) / (beta[1]+x[i][0]));
 }
 
-void calc_jacobian(double b[2], double dst[7][2])
+void calc_jacobian(double beta[2], double dst[7][2])
 {
 	for(int i = 0; i < 7; i++)
 	{
-		dst[i][0] = (-x[i][0]) / (b[1]+x[i][0]);
-		dst[i][1] = pow((b[0]*x[i][0]) / (b[1]+x[i][0]), 2);
+		dst[i][0] = (-x[i][0]) / (beta[1]+x[i][0]);
+		dst[i][1] = (beta[0]*x[i][0]) / pow(beta[1]+x[i][0], 2);
 	}
 }
 
@@ -33,7 +34,7 @@ void get_t_jacobian(double src[7][2], double dst[2][7])
 	for(int i = 0; i < 7; i++)
 	{
 		dst[0][i] = src[i][0];
-		dst[0][i] = src[i][1];
+		dst[1][i] = src[i][1];
 	}
 }
 
@@ -52,23 +53,36 @@ bool get_inverse(double X[2][2], double X_i[2][2])
 	return true;
 }
 
-double update(double b[2], double g[7], double jaco[7][2], double t_jaco[2][7])
+double update(double beta[2], double gamma[7], double jaco[7][2], double t_jaco[2][7], double dst[2])
 {
-	calc_gamma(b, g);
-	calc_jacobian(b, jaco);
+	calc_gamma(beta, gamma);
+	calc_jacobian(beta, jaco);
 	get_t_jacobian(jaco, t_jaco);
-	double temp[2][2], temp_inv[2][2];
+	double temp[2][2]={}, temp_inv[2][2];
 	for(int i = 0; i < 7; ++i)
 	{
-		temp[0][0] += t_jaco[0][i]*jaco[i][0]; 
+		temp[0][0] += t_jaco[0][i]*jaco[i][0];
+		temp[1][0] += t_jaco[1][i]*jaco[i][0];
+		temp[0][1] += t_jaco[0][i]*jaco[i][1];
+		temp[1][1] += t_jaco[1][i]*jaco[i][1];
 	}
-	get_inverse(temp, temp_inv);
+	if(!get_inverse(temp, temp_inv))
+		memcpy(temp_inv, temp, sizeof(temp));
+	temp[0][0] = 0;
+	temp[1][0] = 0;
+	for(int i = 0; i < 7; ++i)
+	{
+		temp[0][0] += t_jaco[0][i]*gamma[i];
+		temp[1][0] += t_jaco[1][i]*gamma[i];
+	}
+	dst[0] = beta[0] + temp_inv[0][0]*temp[0][0] + temp_inv[0][1]*temp[1][0];
+	dst[1] = beta[1] + temp_inv[1][0]*temp[0][0] + temp_inv[1][1]*temp[1][0];
 }
 
 int main(int argc, char **argv)
 {
 	double jaco[7][2], t_jaco[2][7], gamma[7];
-	double	 beta[2] = {1, 1};
+	double	 beta[2] = {0.9, 0.2}, dst[2] = {};
 	FILE	*gp;
 	gp		  = popen("gnuplot -persist", "w");
 	fprintf(gp, "set title \"gauss newton method\"\n");
@@ -79,6 +93,12 @@ int main(int argc, char **argv)
 	{
 		fprintf(gp, "set label %d point pt 7 at %f,%f\n",
 			i+1, x[i][0], x[i][1]);
+	}
+	for(int i = 0; i < 5; ++i)
+	{
+		update(beta, gamma, jaco, t_jaco, dst);
+		memcpy(beta, dst, sizeof(dst));
+		cout << beta[0] << " " << beta[1] << endl;
 	}
 	fprintf(gp, "f(x) = (%f*x) / (%f+x)\n", beta[0], beta[1]);
 	fprintf(gp, "plot f(x)\n");
